b73d61f67673a6754e31a5c90853ca3abc552d43
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2023-08-26 10:50:13 +0100

    check for small coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    
+    # Initialize an array to store the minimum number of coins needed for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    
+    return dp[total] if dp[total] != float('inf') else -1
+
+
